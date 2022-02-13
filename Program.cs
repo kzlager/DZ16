9c93ce2c14283296254f826a4d893879e4614e3f
kzlager <highcolour@yandex.ru>
2022-02-13 18:37:15 +0300

    add files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+int GetRandom()
+{
+    int number = new Random().Next(1, 8);
+    return number;
+}
+int randomazer = GetRandom();
+Console.WriteLine($"Номер дня недели: {randomazer}");
+if (randomazer >= 6) Console.WriteLine($"{randomazer} выходной день");
+else Console.WriteLine($"{randomazer} будний день");
